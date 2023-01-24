d8b093ffe1b1b72aacfa533adf1844b8cd43c921
cheeku07 <64574955+cheeku07@users.noreply.github.com>
2023-01-24 13:03:30 +0530

    Time: 249 ms (53.54%), Space: 75.9 MB (50.51%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    int minimumTime(string s) {
-//         //Lets jump into question
-//         we have 3 options:-
-//             1) remove left 
-//             2) remove right
-//             3)remove any middle one
-//             Now which option can we execute during
-//  a specific condition:
-//         1st condition:- if i am at index where either left index or right index has 1 then i will go with 1 char side.
-//         2nd condition:- if both left and right has 1 one then i will go with both direction one by one.
-//         3rd condition:- if both left and right have 0 thrn here i will try to experiment with all these three steps.
-            
-//     1st major problem:- how to choose that which middle i have to explode first.z
-        
+    int minimumTime(string s) {        
         int n = s.size();
         vector<int> dp(n+1);
-        int step=0,count=0;
         int last=0;
-        if(s[0]=='0') dp[0]=0,count++;
+        if(s[0]=='0') dp[0]=0;
         else dp[0]=1,last=1;
         for(int i=1;i<n;i++){
             if(s[i]=='0'){
-                count++;
+                
                 dp[i]=dp[i-1];
                 
             }
@@ -36,7 +22,6 @@ public:
         else right[n-1]=1,last=1;
         for(int i=n-2;i>=0;i--){
             if(s[i]=='0'){
-                count++;
                 right[i]=right[i+1];
             }
             else{
@@ -48,14 +33,10 @@ public:
         for(int i=0;i<n-1;i++){
             if(s[i]=='1'){
                 ans = min(ans,dp[i]+right[i+1]);
-                //cout<<dp[i]+right[i+1]<<"  "<<i<<"  "<<"yha se 7 aaya h "<<endl;
             }
         }
-       
-        ans=min(ans,dp[n-1]);
-        
-        ans=min(ans,right[0]);
-       if(ans==INT_MAX) return 0;
+     ans=min(ans,dp[n-1]);    
+    ans=min(ans,right[0]);
         return ans;
         
     }
