0206cf8a8dd7eeee650cc08b4cb7732dd41c1c6b
cheeku07 <64574955+cheeku07@users.noreply.github.com>
2023-02-15 08:44:38 +0530

    Time: 33 ms (57.94%), Space: 28.5 MB (46.31%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -5,29 +5,19 @@ public:
         int i=n-1;
         vector<int> ans;
         int carry=0,last,sum;
-        while(k>0 && i>=0){
+        while(k>0 || i>=0){
             last = k%10;
+            if(k>0 && i>=0)
             sum=num[i]+last+carry;
+            else if(i>=0)
+                sum=num[i]+carry;
+            else if(k>0)
+                sum=(k%10)+carry;
             ans.push_back(sum%10);
             if(sum<10) carry=0;
             else carry = sum/10;
             k=k/10;
             i--;
-        }
-        while(i>=0) {
-            sum=num[i]+carry;
-             ans.push_back(sum%10);
-            if(sum<10) carry=0;
-            else carry = sum/10;
-            i--;
-        }
-        while(k>0){
-            sum=(k%10)+carry;
-             ans.push_back(sum%10);
-            if(sum<10) carry=0;
-            else carry = sum/10;
-            
-            k=k/10;
         }
         while(carry>0){
             ans.push_back(carry%10);
