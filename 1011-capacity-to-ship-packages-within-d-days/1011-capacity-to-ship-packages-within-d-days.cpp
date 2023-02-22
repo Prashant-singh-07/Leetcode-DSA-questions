659faa6bf5f38f39da7efde733384cddf8932597
Prashant  kumar singh <64574955+cheeku07@users.noreply.github.com>
2023-02-22 11:42:51 +0530

    Time: 103 ms (10.54%), Space: 27.1 MB (10.78%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    private:
+    bool check(vector<int>& weights,int lim,int days){
+        int sum=0,count=1;
+        int i;
+        for(i=0;i<weights.size();i++){
+            if(count>days) return false;
+            if((sum+weights[i])<=lim){
+                sum+=weights[i];
+            }
+            else{
+                if(weights[i]>lim) return false;
+                sum=weights[i];
+                count++;
+            }
+        }
+        if(count>days) return false;
+        //if(i<weights.size()) return false;
+        return true;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int sum=0;
+        int sz = n/days;
+        if(n%days!=0) sz++;
+        int m = INT_MIN;
+        vector<int> v(n);
+       // v[0]=weights[0];
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
+            if(i-sz+1>0){
+                v[i]=sum-v[i-sz+1];
+            }
+            else{
+                v[i]=sum;
+            }
+            m= max(m,v[i]);
+        }
+        int high=m;
+        int low =1,ans=INT_MAX,mid;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(check(weights,mid,days)){
+                ans=min(ans,mid);
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+       return ans; 
+    }
+};
