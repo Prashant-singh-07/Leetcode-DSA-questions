af1c14fc9b35177020fd6864f688f5cc3d9b9453
Prashant  kumar singh <64574955+cheeku07@users.noreply.github.com>
2023-03-06 00:19:00 +0530

    Time: 197 ms (88.43%), Space: 76.1 MB (56.05%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,65 +1,40 @@
-// class Solution {
-// public:
-//     int minJumps(vector<int>& arr) {
-//         int n = arr.size();
-        
-//         unordered_map<int,vector<int>> m;
-//         for(int i=0;i<arr.size();i++){
-//             m[arr[i]].push_back(i);
-//         }
-//         vector<int> vis(n,INT_MAX);
-//         queue<int> q;
-//         q.push(0);
-//         vis[0]=0;
-//         int k,count=0;
-//         int num,ind;
-//         while(q.size()>0){
-//             k = q.size();
-//             while(k>0){
-//                 ind = q.front();
-//                 if(ind==n-1) return count;
-//                 q.pop();
-//                 if(ind+1<n && vis[ind+1]>count+1){
-//                     if(ind+1==n-1) return count+1;
-//                     q.push(ind+1);
-//                     vis[ind+1]=count+1;
-//                 }
-//                  if(ind-1>=0 && vis[ind-1]>count+1){
-    
-//                     q.push(ind-1);
-//                     vis[ind-1]=count+1;
-//                 }
-//                  if(m[arr[ind]].size()>1){
-//                     for(int j=0;j<m[arr[ind]].size();j++){
-//                         if(m[arr[ind]][j]==n-1) return count+1;
-//                         if(vis[m[arr[ind]][j]]>count+1){
-//                             vis[m[arr[ind]][j]]=count+1;
-//                             q.push(m[arr[ind]][j]);
-//                         }
-//                     }
-//                 }
-//                 k--;
-//             }
-//             count++;
-//         }
-//         return vis[n-1];
-//     }
-// };
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-        unordered_map<int, vector<int>> indicesOfValue;
-        for (int i = 0; i < n; i++)
-            indicesOfValue[arr[i]].push_back(i);
-        vector<bool> visited(n); visited[0] = true;
-        queue<int> q; q.push(0);
-        int step = 0;
-        while (!q.empty()) {
-            for (int size = q.size(); size > 0; --size) {
-                int i = q.front(); q.pop();
-                if (i == n - 1) return step; // Reached to last index
-                vector<int>& next = indicesOfValue[arr[i]];
+        
+        unordered_map<int,vector<int>> m;
+        for(int i=0;i<arr.size();i++){
+            m[arr[i]].push_back(i);
+        }
+        vector<bool> visited(n);
+        queue<int> q;
+        q.push(0);
+        visited[0]=true;
+        int k,count=0;
+        int num,i;
+        while(q.size()>0){
+            k = q.size();
+            while(k>0){
+                i = q.front();
+                if(i==n-1) return count;
+                q.pop();
+                // if(ind+1<n && vis[ind+1]==-1){
+                //     q.push(ind+1);
+                //     vis[ind+1]++;
+                // }
+                //  if(ind-1>0 && vis[ind-1]==-1){
+                //     q.push(ind-1);
+                //     vis[ind-1]++;
+                // }
+                //     for(int j=0;j=m[arr[ind]].size();j++){
+                //         if(vis[m[arr[ind]][j]]==-1){
+                //             vis[m[arr[ind]][j]]++;
+                //             //if(m[arr[ind]][j]==n-1) return count+1;
+                //             q.push(m[arr[ind]][j]);
+                //         }
+                // }
+                 vector<int>& next = m[arr[i]];
                 next.push_back(i - 1); next.push_back(i + 1);
                 for (int j : next) {
                     if (j >= 0 && j < n && !visited[j]) {
@@ -67,10 +42,40 @@ public:
                         q.push(j);
                     }
                 }
-                next.clear(); // avoid later lookup indicesOfValue arr[i]
+                next.clear();
+                k--;
             }
-            step++;
+            count++;
         }
         return 0;
     }
 };
+// class Solution {
+// public:
+//     int minJumps(vector<int>& arr) {
+//         int n = arr.size();
+//         unordered_map<int, vector<int>> indicesOfValue;
+//         for (int i = 0; i < n; i++)
+//             indicesOfValue[arr[i]].push_back(i);
+//         vector<bool> visited(n); visited[0] = true;
+//         queue<int> q; q.push(0);
+//         int step = 0;
+//         while (!q.empty()) {
+//             for (int size = q.size(); size > 0; --size) {
+//                 int i = q.front(); q.pop();
+//                 if (i == n - 1) return step; // Reached to last index
+//                 vector<int>& next = indicesOfValue[arr[i]];
+//                 next.push_back(i - 1); next.push_back(i + 1);
+//                 for (int j : next) {
+//                     if (j >= 0 && j < n && !visited[j]) {
+//                         visited[j] = true;
+//                         q.push(j);
+//                     }
+//                 }
+//                 next.clear(); // avoid later lookup indicesOfValue arr[i]
+//             }
+//             step++;
+//         }
+//         return 0;
+//     }
+// };
