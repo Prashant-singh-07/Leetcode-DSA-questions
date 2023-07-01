2fef087df67788ffce1f38fd47259a2a4486fa8e
Prashant  kumar singh <64574955+Prashant-singh-07@users.noreply.github.com>
2023-07-01 18:29:12 +0530

    Time: 2348 ms (15.09%), Space: 6.8 MB (98.89%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,24 +1,33 @@
 class Solution {
-public:
+    private:
     int ans=INT_MAX;
-    void fun(vector<int>& cookies,int k,int ind,vector<int>& kid){
+    void fun(vector<int>& cookies,int k,int ind,vector<int>& v){
+        
+        int temp=0;
         if(ind==cookies.size()){
-            int m=INT_MIN;
-            for(auto p:kid) m=max(m,p);
-            ans = min(m,ans);
+            for(int i=0;i<k;i++){
+                temp=max(temp,v[i]);
+            }
+            ans = min(ans,temp);
             return ;
         }
         
         for(int i=0;i<k;i++){
-            kid[i]+=cookies[ind];
-            fun(cookies,k,ind+1,kid);
-            kid[i]-=cookies[ind];
+            // I have 2 options here
+            v[i]+=cookies[ind];
+            fun(cookies,k,ind+1,v);
+            v[i]-=cookies[ind];
         }
+        return ;
     }
-    
+public:
     int distributeCookies(vector<int>& cookies, int k) {
-       vector<int> kid(k,0);
-        fun(cookies,k,0,kid);
+      int n = cookies.size();
+        sort(cookies.begin(),cookies.end());
+       // vector<vector<int>> v(k,vector<int>(1<<n,-1));
+        vector<int> v(k);
+        int mask=0;
+        fun(cookies,k,0,v);
         return ans;
     }
 };
