6bc1b75122e9c6229055c5cfb4aed98601fb0fea
Prashant  kumar singh <64574955+Prashant-singh-07@users.noreply.github.com>
2023-09-25 22:06:14 +0530

    Time: 4 ms (48.11%), Space: 7 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-       for(int i=0;i<s.size();i++)
-		t[i+1]+=t[i]-s[i]; //Passing the diff: (t[i]-s[i]) to t[i+1]
-      return t[t.size()-1]; 
+		// for(int i=0;i<s.size();i++)
+		// t[i+1]+=t[i]-s[i]; //Passing the diff: (t[i]-s[i]) to t[i+1]
+		// return t[t.size()-1]; 
+         vector<int> a(26,0);
+        vector<int> b(26,0);
+        for(int i=0;i<s.size();i++){
+            a[s[i]-'a']++;
+            b[t[i]-'a']++;
+        }
+        b[t[t.size()-1]-'a']++;
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]) return 'a'+i;
+        }
+        return 'a';
     }
 };
