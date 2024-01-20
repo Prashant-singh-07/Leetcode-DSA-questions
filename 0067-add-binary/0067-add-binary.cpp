d6d0827b0301766dbd479c4f573c9f548a492b0d
Prashant  kumar singh <64574955+Prashant-singh-07@users.noreply.github.com>
2024-01-20 12:09:38 +0530

    Time: 12 ms (8.29%), Space: 12.4 MB (5.47%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string s = "";
+       int m = a.size();
+        int n = b.size();
+        int rem=0;
         
-        int c = 0, i = a.size() - 1, j = b.size() - 1;
-        while(i >= 0 || j >= 0 || c == 1)
-        {
-            c += i >= 0 ? a[i --] - '0' : 0;
-            c += j >= 0 ? b[j --] - '0' : 0;
-            s = char(c % 2 + '0') + s;
-            c /= 2;
+        int diff = abs(m-n);
+        for(int i=0;i<diff;i++){
+            if(m>n) b = '0'+b;
+            else a = '0'+a;
         }
-        
-        return s;
+        int x;
+        if(m>n) x=m;
+        else x=n;
+        string ans="";
+        while(x>0){
+            cout<<x<<" "<<rem<<endl;
+            if(a[x-1]==b[x-1]){
+                if(rem>0) ans = '1'+ans;
+                else ans = '0'+ans;
+                
+                if(a[x-1]=='0') rem=0; 
+                else rem=1;
+            }
+            else{
+                if(rem>0){
+                    ans = '0'+ans;
+                }
+                else{
+                    ans = '1'+ans;
+                }
+            }
+            x--;
+        }
+        if(rem>0) ans = '1'+ans;
+        return ans;
     }
 };
