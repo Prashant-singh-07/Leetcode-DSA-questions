8eba00490e29d0f9aa0ef8ab68a8b0b9fc5b4ea6
Prashant  kumar singh <64574955+Prashant-singh-07@users.noreply.github.com>
2024-01-20 13:38:19 +0530

    Time: 6 ms (61.55%), Space: 11.6 MB (5.54%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     int countBattleships(vector<vector<char>>& board) {
-         if (board.empty() || board[0].empty()) { return 0; }
-        int m = board.size(), n = board[0].size(), cnt = 0;
-        
-        for (int r = 0; r < m; r++)
-            for (int c = 0; c < n; c++)
-                cnt += board[r][c] == 'X' && (r == 0 || board[r - 1][c] != 'X') && (c == 0 || board[r][c - 1] != 'X');
-        
-        return cnt;
+        int m=board.size();
+        int n = board[0].size();
+        int ans=0,k;
+      for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+              if(board[i][j]=='X'){
+                  ans++;
+                  board[i][j]='p';
+                  if(i+1<m && board[i+1][j]=='X') {
+                      k = i+1;
+                      while(k<m && board[k][j]=='X'){
+                          board[k][j]='p';
+                          k++;
+                      } 
+                  }
+                  else if(j+1<n && board[i][j+1]=='X'){
+                      k=j+1;
+                      while(k<n && board[i][k]=='X'){
+                           board[i][k]='p';
+                          k++;
+                      }
+                  }
+              }
+          }
+          // cout<<board[0][0];
+          // cout<<board[0][3];
+          // cout<<board[1][3];
+          // cout<<board[2][3];
+          // cout<<endl;
+      }
+        return ans;
     }
 };
